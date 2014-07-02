979fe186a33ef3dc51917eec16fc895215a2985f
johntrandall <john@johnrandall.com>
2014-07-02 12:43:03 -0400

    the page shows with debug stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 # == Route Map
 #
-#  Prefix Verb URI Pattern             Controller#Action
-#    root GET  /                       pages#home
-#   about GET  /about(.:format)        pages#show
-# student GET  /students/:id(.:format) students#show
-# project GET  /projects/:id(.:format) projects#show
+# Prefix Verb URI Pattern Controller#Action
+#   root GET  /           pages#home
 #
 
 Rails.application.routes.draw do
-
+  
   root to: "pages#home" # shortcut for the above
 
-  get 'about', to: 'pages#show'
+  # get 'about', to: 'pages#show'
 
 
-  resources :students, only: :show
-  resources :projects, only: :show
+  # resources :students, only: :show
+  # resources :projects, only: :show
 
 end
